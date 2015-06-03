71883c27674b59c15c9312e7908d796106bac8f0
Eduardo <eduardomail@gmail.com>
2015-06-02 23:35:51 -0300

    commit init

---
+++ config/express.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var home = require('../app/routes/home');
+
+
+
+module.exports = function(){
+	var app = express();
+	app.get('/');
+
+	// configuracao de ambiente
+	app.set('port',3000);
+	
+	// middleware
+	app.use(express.static('./public'));
+
+	app.set('view engine','ejs');
+	app.set('views','./app/views');
+	home(app);
+
+	return app;
+};
