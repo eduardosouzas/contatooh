3a63be7f9d8ba5846a35257b22cf956c251b6d1f
Eduardo <eduardomail@gmail.com>
2015-06-07 21:51:04 -0300

    versao ajax
    versao ajax

--- config/express.js
+++ config/express.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var home = require('../app/routes/home');
-
+var load = require('express-load');
+var bodyParser = require('body-parser');
 
 
 module.exports = function(){
@@ -15,7 +15,15 @@ module.exports = function(){
 
 	app.set('view engine','ejs');
 	app.set('views','./app/views');
-	home(app);
+	
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json());
+	app.use(require('method-override')());
+
+	load('models', {cwd: 'app'})
+	.then('controllers')
+	.then('routes')
+	.into(app);
 
 	return app;
 };
---
+++ public/js/controllers/ContatosController.js
@@ -0,0 +1,19 @@
+angular.module('contatooh').controller('ContatosController',['$scope','$http',function($scope,$http) {
+		$scope.total = 0;
+
+		$scope.contatos = [];
+
+		$scope.filtro = ''; 
+
+		$scope.incrementa = function() {
+			$scope.total++;
+		};
+		$http.get('/contatos')
+		.success(function(data) {
+			$scope.contatos = data;
+		})
+		.error(function(statusText) {
+			console.log("Não foi possível obter a lista de contatos");
+			console.log(statusText);
+		});
+	}]);
