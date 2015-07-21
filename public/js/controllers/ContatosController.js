0e9e861154d69e753429f6ad8b85b3f84cb995e1
Eduardo <eduardomail@gmail.com>
2015-07-21 18:14:24 -0300

    finalizando capitulo 5

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,7 +1,34 @@
-angular.module('contatooh').controller('ContatosController',function($scope,$routeParams) {
-	$scope.total = 0;
+angular.module('contatooh').controller('ContatosController',function($scope,Contato) {
+	$scope.contatos = [];
+	$scope.filtro = '';
+    $scope.mensagem = {texto: ''};
 
-	$scope.incrementa = function(){
-	$scope.total++;
-	};
+    function buscaContatos() {
+        Contato.query(
+            function(contatos) {
+                $scope.contatos = contatos;
+                $scope.mensagem = {};
+            },
+            function(erro) {
+                $scope.mensagem = {
+                    texto: 'Não foi possível obter a lista'
+                };
+                console.log(erro);
+            }
+        );
+    }
+    buscaContatos();
+
+    $scope.remove = function(contato) {
+        Contato.delete({id: contato._id},
+            buscaContatos,
+            function(erro) {
+                $scope.mensagem = {
+                    texto: 'Não foi possível remover o contato'
+                };
+                console.log(erro);
+            }
+        );
+    };
+
 });
