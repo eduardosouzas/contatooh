3239e1e757f757c8f32d59f791ab9dca74e687a3
edusouza <eduardomail@gmail.com>
2015-07-20 23:35:17 -0300

    ajax

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,7 +1,26 @@
-angular.module('contatooh').controller('ContatosController',function($scope,$routeParams) {
+angular.module('contatooh').controller('ContatosController',function($scope,$http) {
 	$scope.total = 0;
+
+
+	$scope.contatos  = [];
+	$scope.total = 0;
+	$scope.filtro = '';
 
 	$scope.incrementa = function(){
 	$scope.total++;
 	};
+
+	$http.get('/contatos')
+		.success(function(data) {
+			$scope.contatos = data;
+		})
+		.error(function(statusText) {
+			console.log("Não foi possível obter a lista de+
+            contatos");
+
+
+			console.log(statusText);
+		});
+
+
 });
